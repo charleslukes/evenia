d1a0bd72709e5fcd8ab2450c92556dcb7b1cd31e
charleslukes <charleslukes28@gmail.com>
2023-07-10 23:25:08 +0100

    add fonts from google

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,13 @@ import "@styles/global.scss";
 import Provider from "@modules/Provider";
 import Nav from "@components/Nav";
 import Footer from "@components/Footer";
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-poppins',
+})
 
 type RootLayoutType = {
   children: React.ReactNode;
@@ -9,14 +16,14 @@ type RootLayoutType = {
 
 const RootLayout = ({ children }: RootLayoutType) => {
   return (
-    <html lang="en">
+    <html lang="en" className={poppins.variable}>
       <body>
         <Provider>
           <main className="main">
             <Nav />
             {children}
-          </main>
           <Footer />
+          </main>
         </Provider>
       </body>
     </html>
