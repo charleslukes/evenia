0b2464654637f4549bf43110a8048ccc2ad14a9a
charleslukes <charleslukes28@gmail.com>
2023-07-10 23:27:27 +0100

    update footer component

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,14 +1,35 @@
-import styles from "./styles.module.scss"
+import styles from "./styles.module.scss";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <div>Logo</div>
+      <div className={styles.logo}>
+        <Image
+          src="/assets/icons/logo.svg"
+          alt="evenia logo"
+          width={30}
+          height={30}
+        />
+        <h3>Evenia</h3>
+      </div>
       <div className={styles.list}>
-        <div>Home</div>
-        <div>Create Event</div>
-        <div>Sign Up</div>
-        <div>Explore Events</div>
+        <div>
+          <Link href={"/"} className={styles.link}>
+            Home
+          </Link>
+        </div>
+        <div>
+          <Link href={"/create-event"} className={styles.link}>
+            Create Event
+          </Link>
+        </div>
+        <div>
+          <Link href={"/"} className={styles.link}>
+            Explore Events
+          </Link>
+        </div>
       </div>
     </div>
   );
