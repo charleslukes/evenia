9239a62ca9d37116add20aa89c9be6fc28a5bbe8
charleslukes <charleslukes28@gmail.com>
2023-07-10 12:08:13 +0100

    use nav hook to sign in and sign out user

--- components/Nav/index.tsx
+++ components/Nav/index.tsx
@@ -1,15 +1,34 @@
+"use client";
 import styles from "./styles.module.scss";
+import useNav  from "@hooks/useNav";
 
 const Nav = () => {
+  const {session, providers, handleSignOut, signIn} = useNav();
+ 
   return (
     <nav className={styles.nav}>
       <div>Logo</div>
       <div>
         <input type="text" />
       </div>
-      <div>
-        <button>Sign In</button>
-      </div>
+      {session?.user ? (
+        <div>
+          <button type="button" onClick={handleSignOut}>
+            Sign Out
+          </button>
+        </div>
+      ) : (
+        providers &&
+        Object.values(providers).map((provider) => (
+          <button
+            type="button"
+            key={provider.name}
+            onClick={() => signIn(provider.id)}
+          >
+            Sign In
+          </button>
+        ))
+      )}
     </nav>
   );
 };
