821ee746baa5d189432fb709dc5df29059e11798
charleslukes <charleslukes28@gmail.com>
2023-07-11 18:51:40 +0100

    update detail page

--- app/[eventId]/page.tsx
+++ app/[eventId]/page.tsx
@@ -1,7 +1,10 @@
 import EventDetails from "@components/EventDetails";
+import { eventDetailsPageProp } from "@lib/shared/types";
 
-const EventDetailsPage = () => {
-  return <EventDetails />;
+const EventDetailsPage = ({ params }: eventDetailsPageProp) => {
+  const { eventId } = params;
+
+  return <EventDetails eventId={eventId} />;
 };
 
 export default EventDetailsPage;
