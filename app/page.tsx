fe252f3593c9d5ba0f97e52c147ca305b07ea470
charleslukes <charleslukes28@gmail.com>
2023-07-11 18:51:25 +0100

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,12 @@
 import homeStyles from "@styles/home.module.scss";
-import EventCard from "@components/EventCard";
+import EventCardList from "@components/EventCard";
 
 const Home = () => {
   return (
     <section className={homeStyles.home}>
       <h1>Our events</h1>
       <div className={homeStyles.cards}>
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
+        <EventCardList />
       </div>
     </section>
   );
