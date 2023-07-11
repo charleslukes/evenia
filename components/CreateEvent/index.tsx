7a31c790f33fba6370824a06fd29db379a538f71
charleslukes <charleslukes28@gmail.com>
2023-07-11 18:55:04 +0100

    update create event component

--- components/CreateEvent/index.tsx
+++ components/CreateEvent/index.tsx
@@ -1,8 +1,49 @@
-import Textarea from "@molecules/Textarea";
+"use client";
+import Textarea from "@ui/Textarea";
 import styles from "./styles.module.scss";
-import TextInput from "@molecules/Text";
+import TextInput from "@ui/Text";
+import Button from "@ui/Button";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { INewSession } from "@lib/shared/types";
 
 const CreateEvent = () => {
+  const { data: session } = useSession();
+  const ownerSession = session as INewSession;
+
+  const router = useRouter();
+
+  useEffect(() => {
+    // have a loading state before routing
+    if (!session?.user) {
+      router.push("/");
+    }
+  }, []);
+
+  const createEvent = async () => {
+    try {
+      const res = await fetch("/api/events/new", {
+        method: "POST",
+        body: JSON.stringify({
+          name: "Outline Intro",
+          date: new Date(),
+          city: "Lagos",
+          title: "How do men think",
+          desc: "Learn about men and how they think in many ways",
+          image: "/assets/images/landscape2.jpeg",
+          ownerId: ownerSession.ownerId,
+        }),
+      });
+      if (res.ok) {
+        alert("event created");
+        router.push("/");
+      }
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+
   return (
     <div className={styles.container}>
       <div>
@@ -32,7 +73,12 @@ const CreateEvent = () => {
           </div>
         </div>
         <div>
-          <button>Continue</button>
+          <Button
+            type="button"
+            text="Continue"
+            className={styles.contBtn}
+            handleClick={createEvent}
+          />
         </div>
       </form>
     </div>
