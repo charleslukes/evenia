1fc2445424688462467c8b8387435ab770a0c959
charleslukes <charleslukes28@gmail.com>
2023-07-12 20:01:43 +0100

    map dropzone and react select to hooks form

--- components/EventForm/index.tsx
+++ components/EventForm/index.tsx
@@ -2,49 +2,51 @@ import Textarea from "@ui/Textarea";
 import styles from "./styles.module.scss";
 import TextInput from "@ui/Text";
 import Button from "@ui/Button";
-import { useForm, SubmitHandler } from "react-hook-form";
+import { Controller } from "react-hook-form";
 import Select from "react-select";
 import { CloudArrowUp } from "phosphor-react";
+import Dropzone from "react-dropzone";
+import useEventForm from "@lib/hooks/useEventForm";
+import { eventFormPropType } from "@lib/shared/types";
 
-type Inputs = {
-  category: string;
-  desc: string;
-  date: string;
-  location: string;
-  title: string;
-  price: string;
-};
-
-const EventForm = () => {
+const EventForm = ({ submit }: eventFormPropType) => {
   const {
-    register,
     handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm<Inputs>();
-
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
-  
-  const options = [
-    { value: "paris", label: "paris" },
-    { value: "new york", label: "new york" },
-    { value: "instanbul", label: "instanbul" },
-    { value: "london", label: "london" },
-    { value: "madrid", label: "madrid" },
-    { value: "tokyo", label: "tokyo" },
-    { value: "dubia", label: "dubia" },
-    { value: "bilda", label: "bilda" },
-    { value: "wakanda", label: "wakanda" },
-    { value: "gotham", label: "gotham" },
-  ];
-
-
+    onDrop,
+    register,
+    categoryOption,
+    control,
+    locationOptions,
+  } = useEventForm();
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit(submit)}>
       <div className={styles.formInput}>
         <div className={styles.left}>
-          <CloudArrowUp size={40} />
+          <Controller
+            name="image"
+            control={control}
+            render={({ field: { onChange } }) => {
+              return (
+                <Dropzone
+                  accept={{
+                    "image/jpeg": [],
+                    "image/png": [],
+                  }}
+                  onDrop={onDrop}
+                >
+                  {({ getRootProps, getInputProps }) => (
+                    <section>
+                      <div {...getRootProps()}>
+                        <input {...getInputProps()} />
+                        <CloudArrowUp size={40} />
+                      </div>
+                    </section>
+                  )}
+                </Dropzone>
+              );
+            }}
+          />
         </div>
         <div className={styles.right}>
           <div className={`${styles.firstDetails} ${styles.marginBottom}`}>
@@ -53,38 +55,59 @@ const EventForm = () => {
               register={register("title", { required: true })}
               className={styles.titleInput}
             />
-            <Select options={options} id={"1"} />
+            <Controller
+              control={control}
+              defaultValue={categoryOption[4].value}
+              name="category"
+              render={({ field: { onChange, value, ref } }) => {
+                return (
+                  <Select
+                    classNamePrefix="addl-class"
+                    options={categoryOption}
+                    value={categoryOption.find((c) => c.value === value)}
+                    onChange={(val) => val && onChange(val.value)}
+                  />
+                );
+              }}
+            />
           </div>
           <div className={styles.marginBottom}>
             <Textarea register={register("desc", { required: true })} />
           </div>
-          <div className={styles.marginBottom}>
+          <div className={`${styles.firstDetails} ${styles.marginBottom}`}>
             <TextInput
-              placeholder="price in USD"
+              placeholder="Price in USD $"
               register={register("price", { required: true })}
+              type="number"
             />
-          </div>
 
-          <div className={styles.marginBottom}>
             <TextInput
               placeholder="date"
+              type="date"
               register={register("date", { required: true })}
             />
           </div>
           <div className={styles.marginBottom}>
-            <TextInput
-              placeholder="location"
-              register={register("location", { required: true })}
+            <Controller
+              control={control}
+              defaultValue={locationOptions[3].value}
+              name="location"
+              render={({ field: { onChange, value, ref } }) => {
+                return (
+                  <Select
+                    classNamePrefix="addl-class"
+                    options={locationOptions}
+                    value={locationOptions.find((c) => c.value === value)}
+                    onChange={(val) => val && onChange(val.value)}
+                  />
+                );
+              }}
             />
           </div>
         </div>
       </div>
       <div>
-        <Button
-          type="submit"
-          text="Continue"
-          className={styles.contBtn}
-        />
+        <Button type="submit" text="Continue" className={styles.contBtn} />
       </div>
     </form>
   );
