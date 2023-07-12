0b571794319319fd3586c52d89e4100795293ece
charleslukes <charleslukes28@gmail.com>
2023-07-12 20:15:32 +0100

    update event update template

--- components/UpdateEvent/index.tsx
+++ components/UpdateEvent/index.tsx
@@ -1,45 +1,10 @@
 "use client";
 import styles from "./styles.module.scss";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-import { INewSession } from "@lib/shared/types";
 import EventForm from "@components/EventForm";
+import useUpdateEvent from "@lib/hooks/useUpdateEvent";
 
 const UpdateEvent = () => {
-  const { data: session } = useSession();
-  const ownerSession = session as INewSession;
-  const router = useRouter();
-
-  useEffect(() => {
-    // have a loading state before routing
-    if (!session?.user) {
-      router.push("/");
-    }
-  }, []);
-
-  const updateEvent = async () => {
-    try {
-      const res = await fetch("/api/events/new", {
-        method: "PUT",
-        body: JSON.stringify({
-          name: "Outline Intro",
-          date: new Date(),
-          city: "Lagos",
-          title: "How do men think",
-          desc: "Learn about men and how they think in many ways",
-          image: "/assets/images/landscape2.jpeg",
-          ownerId: ownerSession.ownerId,
-        }),
-      });
-      if (res.ok) {
-        alert("event created");
-        router.push("/");
-      }
-    } catch (error) {
-      console.log({ error });
-    }
-  };
+  const {updateEvent} = useUpdateEvent()
 
   return (
     <div className={styles.container}>
@@ -47,7 +12,7 @@ const UpdateEvent = () => {
         <h2>Event</h2>
         <small>Update your event</small>
       </div>
-      <EventForm />
+      <EventForm submit={updateEvent} />
     </div>
   );
 };
