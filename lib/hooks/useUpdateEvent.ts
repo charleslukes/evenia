aabec85a94497576e256e987ba5f38eae1e66a0d
charleslukes <charleslukes28@gmail.com>
2023-07-12 21:21:52 +0100

    fetch one event

--- lib/hooks/useUpdateEvent.ts
+++ lib/hooks/useUpdateEvent.ts
@@ -1,12 +1,20 @@
-import { INewSession, formInputTypes } from "@lib/shared/types";
+import { INewSession, eventRes, formInputTypes } from "@lib/shared/types";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
-const useUpdateEvent = () => {
+const useUpdateEvent = (eventId: string) => {
   const { data } = useSession();
   const [session, setSession] = useState<INewSession>(data as INewSession);
   const router = useRouter();
+  const [ownerEvent, setOwnerEvent] = useState<formInputTypes>();
+  
+  const fetchEvent = async () => {
+    const response = await fetch(`/api/events/${eventId}`);
+    const data: eventRes = await response.json();
+    const {owner, ...others} = data;
+    setOwnerEvent(others);
+  };
 
   useEffect(() => {
     if (data) {
@@ -14,15 +22,18 @@ const useUpdateEvent = () => {
     }
   }, [data]);
 
+  useEffect(() => {
+    fetchEvent()
+  }, [eventId])
+
   const updateEvent = async (data: formInputTypes) => {
     try {
       const newData = {
         ...data,
-        ownerId: session.ownerId,
-        date: new Date(data.date),
+        ownerId: session.ownerId
       };
       const res = await fetch("/api/events/new", {
-        method: "POST",
+        method: "PUT",
         body: JSON.stringify(newData),
       });
       if (res.ok) {
@@ -36,7 +47,8 @@ const useUpdateEvent = () => {
   };
 
   return {
-    updateEvent
+    updateEvent,
+    ownerEvent
   };
 };
 
