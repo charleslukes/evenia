7cb4e5ad8cf3c79875d78cc7654fe528236ee72a
charleslukes <charleslukes28@gmail.com>
2023-07-12 21:22:07 +0100

    implement update event

--- components/UpdateEvent/index.tsx
+++ components/UpdateEvent/index.tsx
@@ -2,9 +2,13 @@
 import styles from "./styles.module.scss";
 import EventForm from "@components/EventForm";
 import useUpdateEvent from "@lib/hooks/useUpdateEvent";
+import { useSearchParams } from "next/navigation";
 
 const UpdateEvent = () => {
-  const {updateEvent} = useUpdateEvent()
+  const searchParams = useSearchParams();
+  const eventId = searchParams.get("eventId");
+
+  const { updateEvent, ownerEvent } = useUpdateEvent(eventId!);
 
   return (
     <div className={styles.container}>
@@ -12,7 +16,13 @@ const UpdateEvent = () => {
         <h2>Event</h2>
         <small>Update your event</small>
       </div>
-      <EventForm submit={updateEvent} />
+      {
+        ownerEvent ? <EventForm
+        submit={updateEvent}
+        defaultValues={ownerEvent}
+      /> : <div>Fetching event pls wait...</div>
+      }
+      
     </div>
   );
 };
