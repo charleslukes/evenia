dfacc56217d8889e59f748e6bf154bf8a039a55e
charleslukes <charleslukes28@gmail.com>
2023-07-12 20:11:38 +0100

    update create event template

--- components/CreateEvent/index.tsx
+++ components/CreateEvent/index.tsx
@@ -1,38 +1,10 @@
 "use client";
-import {useState, useEffect} from "react";
 import styles from "./styles.module.scss";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { INewSession, formInputTypes } from "@lib/shared/types";
 import EventForm from "@components/EventForm";
+import useCreateEvent from "@lib/hooks/useCreateEvent";
 
 const CreateEvent = () => {
-  const { data } = useSession();
-  const [session, setSession] = useState<INewSession>(data as INewSession)
-  const router = useRouter();
-
-  useEffect(() => {
-    if(data) {
-      setSession(data as INewSession)
-    }
-  }, [data])
-
-  const createEvent = async (data: formInputTypes) => {
-    try {
-      const newData = {...data, ownerId: session.ownerId, date: new Date(data.date) }
-      const res = await fetch("/api/events/new", {
-        method: "POST",
-        body: JSON.stringify(newData),
-      });
-      if (res.ok) {
-        alert("event created");
-        router.push("/");
-      }
-    } catch (error) {
-      alert(error);
-      console.log({ error });
-    }
-  };
+ const {createEvent} = useCreateEvent()
 
   return (
     <div className={styles.container}>
