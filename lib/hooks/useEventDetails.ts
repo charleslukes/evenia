a2a6d4a676d92d7e66be4ff2fd0a12b3f49e546e
charleslukes <charleslukes28@gmail.com>
2023-07-12 14:53:44 +0100

    create event details hook

--- lib/hooks/useEventDetails.ts
+++ lib/hooks/useEventDetails.ts
@@ -1,8 +1,48 @@
-const useEventDetails = () => {
+import { INewSession, eventDetailProp, eventRes } from "@lib/shared/types";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
-    return {
+const useEventDetails = ({ eventId }: eventDetailProp) => {
+  const { data: session } = useSession();
+  const [isOwner, setIsOwner] = useState<boolean>(false);
+  const [eventData, setEventData] = useState<eventRes>({} as eventRes);
+  const router = useRouter();
 
+  const fetchEvent = async () => {
+    const userSession = session as INewSession;
+    const response = await fetch(`/api/events/${eventId}`);
+    const data = await response.json();
+    setIsOwner(Number(data.ownerId) === Number(userSession?.ownerId));
+    setEventData(data);
+  };
+
+  const handleUpdateEvent = () => {
+    router.push(`/update-event/${eventData.id}`);
+  };
+
+  const deleteEvent = async () => {
+    try {
+      const res = await fetch(`/api/events/${eventId}`, { method: "DELETE" });
+      if (res.ok) {
+        alert("event deleted");
+        router.push("/");
+      }
+    } catch (error) {
+      console.log({ error });
     }
-}
+  };
+
+  useEffect(() => {
+    fetchEvent();
+  }, [session]);
+
+  return {
+    deleteEvent,
+    handleUpdateEvent,
+    isOwner,
+    eventData,
+  };
+};
 
 export default useEventDetails;
