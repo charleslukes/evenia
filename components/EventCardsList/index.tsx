b5e50a9dcf646d877e0dc0eb13e38e2bb52d3d2d
charleslukes <charleslukes28@gmail.com>
2023-07-13 02:53:18 +0100

    update card

--- components/EventCardsList/index.tsx
+++ components/EventCardsList/index.tsx
@@ -2,39 +2,46 @@
 import styles from "./styles.module.scss";
 import Image from "next/image";
 import useEventCards from "@lib/hooks/useEventCards";
+import { dateRange } from "@lib/utils/date";
 
 const EventCardsList = () => {
-  const { allEvents, handleClick, dateRange, titleRange } = useEventCards();
+  const { allEvents, handleClick, titleRange } = useEventCards();
 
   return (
     <>
-      {allEvents.map((data, i) => {
-        return (
-          <div
-            key={i}
-            className={styles.container}
-            onClick={() => handleClick(data.id.toString())}
-          >
-            <div>
-              <Image
-                src={data.image ? data.image : "/assets/images/landscape3.jpeg"}
-                width={0}
-                height={0}
-                alt="an event card"
-                className={styles.image}
-                sizes="100vw"
-              />
-            </div>
-            <div className={styles.subcontainer}>
-              <div className={styles.card_date}>{dateRange(data.date)}</div>
-              <div className={styles.title}>{titleRange(data.desc)}</div>
-              <div className={styles.ownerName}>
-                <small>{data.owner.name}</small>
+      {allEvents.length ? (
+        allEvents.map((data, i) => {
+          return (
+            <div
+              key={i}
+              className={styles.container}
+              onClick={() => handleClick(data.id.toString())}
+            >
+              <div>
+                <Image
+                  src={
+                    data.image ? data.image : "/assets/images/landscape3.jpeg"
+                  }
+                  width={0}
+                  height={0}
+                  alt="an event card"
+                  className={styles.image}
+                  sizes="100vw"
+                />
+              </div>
+              <div className={styles.subcontainer}>
+                <div className={styles.card_date}>{dateRange(data.date)}</div>
+                <div className={styles.title}>{titleRange(data.title)}</div>
+                <div className={styles.ownerName}>
+                  <small>{data.owner.name}</small>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })
+      ) : (
+        <div>Fetching...</div>
+      )}
     </>
   );
 };
